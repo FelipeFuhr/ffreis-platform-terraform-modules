204e716f0663160b7c715f4421bb6187c2c2530d
FelipeFuhr <felipefuhr7@gmail.com>
2026-03-29 23:41:22 -0300

    fmt: terraform

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -99,14 +99,14 @@ resource "aws_lambda_function" "this" {
   kms_key_arn                    = var.kms_key_arn
 
   dynamic "environment" {
-    for_each = length(var.environment_variables) > 0 ? {"enabled" = true} : {}
+    for_each = length(var.environment_variables) > 0 ? { "enabled" = true } : {}
     content {
       variables = var.environment_variables
     }
   }
 
   dynamic "vpc_config" {
-    for_each = length(var.vpc_subnet_ids) > 0 ? {"enabled" = true} : {}
+    for_each = length(var.vpc_subnet_ids) > 0 ? { "enabled" = true } : {}
     content {
       subnet_ids         = var.vpc_subnet_ids
       security_group_ids = var.vpc_security_group_ids
