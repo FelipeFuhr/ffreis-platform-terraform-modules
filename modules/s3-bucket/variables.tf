45c865ee04e8dd179e63ae4d47cec53b374d0ccd
FelipeFuhr <felipefuhr7@gmail.com>
2026-03-29 22:07:38 -0300

    fix: ci

--- modules/s3-bucket/variables.tf
+++ modules/s3-bucket/variables.tf
@@ -59,9 +59,8 @@ variable "abort_incomplete_multipart_upload_days" {
 }
 
 variable "logging_target_bucket" {
-  description = "S3 bucket to receive access logs. Leave empty to skip access logging."
+  description = "S3 bucket to receive access logs. Pass an empty string only for the logging bucket itself to break the circular dependency."
   type        = string
-  default     = ""
 }
 
 variable "logging_target_prefix" {
